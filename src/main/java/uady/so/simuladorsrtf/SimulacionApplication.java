f8f2d46c822fbcf482f8aec9aae4edc2e1aea1ed
ErickVega57 <A21203354@alumnos.uady.mx>
2026-05-13 13:46:52 -0600

    cambios de nombre clase principal

--- src/main/java/uady/so/simuladorsrtf/SimulacionApplication.java
+++ src/main/java/uady/so/simuladorsrtf/SimulacionApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SimulacionApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SimulacionApplication.class.getResource("view/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 604, 522);
         stage.setTitle("Simulador de algoritmo SRJF");
         stage.setScene(scene);
